051f287d5e1ee681d135b621e3ed4e30a6c86678
Casey Icenhour <cticenho@ncsu.edu>
2017-10-13 11:11:41 -0400

    Reorganized, started coding of dielectric tensor system. Only vacuum and constant epsilon_r versions ready.

---
+++ functions/dielectricTensor.m
@@ -0,0 +1,16 @@
+% Dielectric tensor definition function
+% Last edit: 10-12-2017
+% Editor: Casey Icenhour (NCSU)
+
+function tensor = dielectricTensor(type,const)
+
+    if strcmp(type,'vacuum')
+        tensor = eye(3);
+    elseif strcmp(type,'constant')
+        tensor = const*eye(3);
+    else
+        errmsg('The only currently valid tensor types are vacuum and const!')
+    end
+
+end
+
